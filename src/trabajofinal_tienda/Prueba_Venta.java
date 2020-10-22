5050c675e46138c4d998d51d8f8d59dc44e88d99
Sil <73064534+silvinel@users.noreply.github.com>
2020-10-22 08:47:08 -0300

    Create Prueba_Venta.java
    creado desde git

---
+++ src/trabajofinal_tienda/Prueba_Venta.java
@@ -0,0 +1,15 @@
+//TRABAJO FINAL CASO Nro 2 TIENDA - Fecha de entrega 25-10-2020
+//GRUPO : HERNAN FIGUEROA, FERNANDO BARRIOS, ALEXANDER MARINO, LEANDRO TAPPA, SILVINA RONZONI
+package trabajofinal_tienda;
+public class Prueba_Venta {
+  public static void main(String[] args ){
+                  
+    // INSTANCIAR OBJETOS VENTA CON MATRIZ     
+     Venta venta1 = new Venta(0,0,0,0,0,0);
+     
+     
+    //LLAMO AL METODO PARA CARGAR VENTAS QUE ESTA EN LA CLASE VENTA
+     venta1.HagoVenta();
+         
+   }  
+}
